1389cf5a9d6f03bdf297e026eceb3f4d561b948e
Andrew Starr <astarr93@outlook.com>
2020-10-20 04:40:48 -0400

    Create orm.js

---
+++ config/orm.js
@@ -0,0 +1,18 @@
+const connection = require('./connection');
+
+const orm = {
+
+    selectAll: function selectAll() {
+        connection.
+    },
+
+    insertOne: function insertOne() {
+
+    },
+
+    updateOne: function updateOne() {
+
+    }
+};
+
+module.exports = orm;
