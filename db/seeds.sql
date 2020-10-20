e8385df4a61ad2c6d29baf1d147e1098863ae36b
Andrew Starr <astarr93@outlook.com>
2020-10-20 03:37:34 -0400

    Created and updated seeds and schema
    Updated schema and seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+USE burgers_db
+
+INSERT INTO burgers
+    (id, burger_name, devoured)
+VALUES
+    (1, 'Bacon Jam Burger', devoured),
+    (2, 'QTR Pounder with Cheese', devoured),
+    (3, 'Burbon Cinnamon Angus', devoured);
