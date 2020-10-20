2fd39dd028d34bf0920b2b236942849aa28b439e
Andrew Starr <astarr93@outlook.com>
2020-10-20 03:46:08 -0400

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -4,3 +4,18 @@ const mysql = require('mysql');
 
 // Prep local SQL database connection by reading .env
 dotenv.config();
+const connection = mysql.createConnection({
+    host: process.env.host,
+    port: process.env.port,
+    user: process.env.user,
+    password: process.env.password,
+    database: process.env.database
+});
+
+// Create connection to mysql database
+connection.connect((err) => {
+    if (err) throw err;
+});
+
+// Export connection
+module.exports = connection;
