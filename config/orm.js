97309b52e69a53b96281033aa62e97f366dc634d
Andrew Starr <astarr93@outlook.com>
2020-10-21 19:54:46 -0400

    Update orm.js

--- config/orm.js
+++ config/orm.js
@@ -3,11 +3,17 @@ const connection = require('./connection');
 const orm = {
 
     selectAll: function selectAll() {
-        connection.
+        const query = `SELECT * FROM ${tableHistory}`;
+        connection.query(query, function (err, result) {
+            if (err) {
+                throw err;
+            }
+            return(result);
+        });
     },
 
     insertOne: function insertOne() {
-
+        const query = ``
     },
 
     updateOne: function updateOne() {
