7b6392774189d9aeb36ed04ffcadfbc5ca26f690
Andrew Starr <astarr93@outlook.com>
2020-10-23 06:30:27 -0400

    Linked JS to main.handlebars
    updated handlebars vars and controller routes. Linked javascript up to handlesbars main

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -12,7 +12,7 @@ router.get("/", function (req, res) {
     });
 });
 
-router.post("/api/burger", function (req, res) {
+router.post("/api/burgers", function (req, res) {
     burger.insertOne(req.body, function (result) {
         if (result.changedRows === 0) {
             return res.status(404).end();
--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,20 +1,18 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function () {
-    $(".eat").on("click", function (event) {
+    $(".eatBurger").on("click", function (event) {
         var id = $(this).data("id");
         var devoured = { devoured: true, };
 
-        // console.log("HERE")
-
-        Send the POST request.
-            $.ajax("/api/burgers/" + id, {
-                type: "POST",
-                data: devoured,
-            }).then(
-                function () {
-                    location.reload();
-                }
-            );
+        // Send the POST request.
+        $.ajax("/api/burgers/" + id, {
+            type: "POST",
+            data: devoured,
+        }).then(
+            function () {
+                location.reload();
+            }
+        );
     });
 
     $(".create-form").on("submit", function (event) {
@@ -22,7 +20,7 @@ $(function () {
         event.preventDefault();
 
         var newBurger = {
-            burger_name: $("#devoured").val().trim(),
+            burger_name: $("#addBurger").val().trim(),
             devoured: 0,
         };
 
