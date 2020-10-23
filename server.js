bd31ed9671f2831f1a74083193bf5413706d819e
Andrew Starr <astarr93@outlook.com>
2020-10-23 02:43:30 -0400

    Update server.js
    change quotes for personal taste

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const express = require("express");
+const express = require('express');
 
 const PORT = process.env.PORT || 3000;
 
@@ -9,10 +9,10 @@ app.use(express.static(`${__dirname}/public`));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const exphbs = require("express-handlebars");
+const exphbs = require('express-handlebars');
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
 const routes = require("./controllers/burgers_controller.js");
 
