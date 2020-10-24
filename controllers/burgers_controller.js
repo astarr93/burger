29c116dd810e8f24b0791e96db69a5139226cc78
Andrew Starr <astarr93@outlook.com>
2020-10-23 20:01:39 -0400

    Update burgers_controller.js
    added console.log for testing

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -24,7 +24,7 @@ router.post("/api/burgers", function (req, res) {
 
 router.put("/api/burgers/:id", function (req, res) {
     const condition = { id: req.params.id };
-
+    console.log("4");
     burger.updateOne({ devoured: true }, condition, function (result) {
         if (result.changedRows === 0) {
             return res.status(404).end();
